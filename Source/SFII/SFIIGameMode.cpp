23555820aff6bd95b392c0d3365f4ae4f1d701a3
Isaiah Che <hunter66888@gmail.com>
2020-07-29 14:45:44 -0400

    Modified asset file paths

--- Source/SFII/SFIIGameMode.cpp
+++ Source/SFII/SFIIGameMode.cpp
@@ -1,12 +1,7 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "SFIIGameMode.h"
-#include "SFIICharacter.h"
-#include "SFIIGameInstance.h"
-#include "Kismet/GameplayStatics.h"
-#include "AIController.h"
-#include "SFIICamera.h"
-#include "SFIIGameInstance.h"
+
 
 ASFIIGameMode::ASFIIGameMode()
 {
